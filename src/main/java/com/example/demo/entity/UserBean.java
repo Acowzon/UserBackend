1160dde73d20d0388114923e992764f5a8b1bf87
syboomsy <1378681102@qq.com>
2021-04-05 11:07:59 +0800

    new user bean

--- src/main/java/com/example/demo/entity/UserBean.java
+++ src/main/java/com/example/demo/entity/UserBean.java
@@ -3,92 +3,131 @@ package com.example.demo.entity;
 import java.sql.Date;
 
 public class UserBean {
+	private String userId;  // 用户id
+	private String userName;    // 登录用户名
+	private String userPwd;     // 登录密码
+	private String userRealname;    // 用户真实姓名
+	private String userNickname;    // 用户昵称
+	private String userImage;   // 用户头像的路径
+	private String Phone;   // 用户电话
+	private String userEmail;   // 用户邮箱
+	private String sex; // 用户性别 男 女
+	private Date userBirth; // 用户生日
+	private int userType;   // 用户类型，0买家，1卖家
+	private String userAddressId;   // 用户地址
+	private Date createTime;    // 用户创建时间
+	private Date updateTime;    // 用户修改时间
 
-    private String userID;
-    private String userName;
-    private String userPhone;
-    private String userEmail;
-    private String passWord;
-    private int userType;
 
-    private Date createDate;
-    private Date updateDate;
+	public String getUserId() {
+		return userId;
+	}
 
-    public String getUserID() {
-        return userID;
-    }
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
 
-    public void setUserID(String userID) {
-        this.userID = userID;
-    }
+	public String getUserName() {
+		return userName;
+	}
 
-    public String getUserName() {
-        return userName;
-    }
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+	public String getUserPwd() {
+		return userPwd;
+	}
 
-    public String getUserPhone() {
-        return userPhone;
-    }
+	public void setUserPwd(String userPwd) {
+		this.userPwd = userPwd;
+	}
 
-    public void setUserPhone(String userPhone) {
-        this.userPhone = userPhone;
-    }
+	public String getUserRealname() {
+		return userRealname;
+	}
 
-    public String getUserEmail() {
-        return userEmail;
-    }
+	public void setUserRealname(String userRealname) {
+		this.userRealname = userRealname;
+	}
 
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
+	public String getUserNickname() {
+		return userNickname;
+	}
 
-    public String getPassWord() {
-        return passWord;
-    }
+	public void setUserNickname(String userNickname) {
+		this.userNickname = userNickname;
+	}
 
-    public void setPassWord(String passWord) {
-        this.passWord = passWord;
-    }
+	public String getUserImage() {
+		return userImage;
+	}
 
-    public int getUserType() {
-        return userType;
-    }
+	public void setUserImage(String userImage) {
+		this.userImage = userImage;
+	}
 
-    public void setUserType(int userType) {
-        this.userType = userType;
-    }
+	public String getPhone() {
+		return Phone;
+	}
 
-    public Date getCreateDate() {
-        return createDate;
-    }
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
 
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
+	public String getUserEmail() {
+		return userEmail;
+	}
 
-    public Date getUpdateDate() {
-        return updateDate;
-    }
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
 
-    public void setUpdateDate(Date updateDate) {
-        this.updateDate = updateDate;
-    }
+	public String getSex() {
+		return sex;
+	}
 
-    @Override
-    public String toString() {
-        return "UserPojoImpl{" +
-                "userID=" + userID +
-                ", userName='" + userName + '\'' +
-                ", userPhone='" + userPhone + '\'' +
-                ", userEmail='" + userEmail + '\'' +
-                ", passWord='" + passWord + '\'' +
-                ", userType=" + userType +
-                ", createDate=" + createDate +
-                ", updateDate=" + updateDate +
-                '}';
-    }
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+	public Date getUserBirth() {
+		return userBirth;
+	}
+
+	public void setUserBirth(Date userBirth) {
+		this.userBirth = userBirth;
+	}
+
+	public int getUserType() {
+		return userType;
+	}
+
+	public void setUserType(int userType) {
+		this.userType = userType;
+	}
+
+	public String getUserAddressId() {
+		return userAddressId;
+	}
+
+	public void setUserAddressId(String userAddressId) {
+		this.userAddressId = userAddressId;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
 }
